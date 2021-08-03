84fb2cc0c25a6f5ee101912c6da45043eb72ea95
Shintaro-PC <shintaro0dev@gmail.com>
2021-08-03 19:51:23 -0300

    CorsFilter

---
+++ src/main/java/com/shintaro/genericAPI/cors/CorsFilter.java
@@ -0,0 +1,44 @@
+package com.shintaro.genericAPI.cors;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+	
+	private String originPermitida = "http://localhost:8000";
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		res.setHeader("Access-Control-Allow-Origin", originPermitida);
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		
+		if("OPTIONS".equals(req.getMethod()) && originPermitida.equals(req.getHeader("Origin"))) {
+			res.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE,OPTIONS");
+			res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-type, Accept");
+			res.setHeader("Access-Control-Max-Age", "3600");
+			res.setStatus(HttpServletResponse.SC_OK);
+		}else {
+			chain.doFilter(req, res);
+		}
+		
+	}
+
+}
