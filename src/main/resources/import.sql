831cb9addd5050bbdce1d1efc1afb43b205df376
Shintaro-PC <shintaro0dev@gmail.com>
2021-08-05 10:56:49 -0300

    Auto default user insert

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+/*
+ *	Auto Seeder
+ *  
+ */
+
+
+/*#######################################
+ *  
+ * User Insert
+ * 
+ * ######################################
+ */
+
+insert into user values("3f00f2d2-f53a-11eb-9bd5-025076325304","$2a$10$zKNsiyEoSAIDKNClNGWN3uXuhj3YBD3GRONBkvRsIl4GfVkF8/km2","ADMIN","admin")
