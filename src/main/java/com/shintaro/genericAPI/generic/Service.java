80911b4b524b847a6ce9395b093915214a2ea49c
Shintaro-PC <shintaro0dev@gmail.com>
2021-08-05 10:58:23 -0300

    Added edit and delete functions

--- src/main/java/com/shintaro/genericAPI/generic/Service.java
+++ src/main/java/com/shintaro/genericAPI/generic/Service.java
@@ -3,7 +3,11 @@ package com.shintaro.genericAPI.generic;
 import java.util.List;
 import java.util.UUID;
 
-public class Service<entity extends Object ,repository extends Repository<entity>> {
+import org.springframework.beans.BeanUtils;
+
+import com.shintaro.genericAPI.filters.PropertyFilter;
+
+public class Service<entity extends Object ,repository extends Repository<entity>> extends PropertyFilter {
 	
 	private repository repository;
 	
@@ -16,7 +20,7 @@ public class Service<entity extends Object ,repository extends Repository<entity
 	}
 
 	public entity getByID(UUID iD){
-		System.out.println(repository.findById(iD).orElseThrow(() -> new IllegalArgumentException(String.format("missing user:%s", iD))));
+		//System.out.println(repository.findById(iD).orElseThrow(() -> new IllegalArgumentException(String.format("missing user:%s", iD))));
 		return repository.findById(iD).get();
 	}
 
@@ -24,5 +28,14 @@ public class Service<entity extends Object ,repository extends Repository<entity
 		return repository.saveAndFlush(model);
 	}
 	
+	public void delete(UUID iD) {
+		repository.deleteById(iD);
+	}
+
+	public entity edit(UUID iD, entity model) {
+		entity savedData = getByID(iD);
+		BeanUtils.copyProperties(model, savedData, getNullPropertyNames(model));
+		return repository.save(savedData);
+	}
 
 }
