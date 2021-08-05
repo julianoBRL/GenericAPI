a4d1cb5613fb2bf73ba65630d149741ae2aa72b3
Shintaro-PC <shintaro0dev@gmail.com>
2021-08-05 10:56:22 -0300

    Added ChachedController and new routes

--- src/main/java/com/shintaro/genericAPI/generic/CachedController.java
+++ src/main/java/com/shintaro/genericAPI/generic/CachedController.java
@@ -1,5 +1,6 @@
 package com.shintaro.genericAPI.generic;
 
+import java.lang.reflect.Field;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
@@ -12,38 +13,67 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-public interface Controller<
+public interface CachedController<
 	entity extends Object,
 	repository extends Repository<entity>,
-	service extends Service<entity,repository>
+	service extends Service<entity,repository>,
+	singletron extends Singletron<entity>
 > {
-
+	
 	service getService();
+	singletron getSingletron();
 	
 	@GetMapping
 	default ResponseEntity<?> getAll() {
-		return ResponseEntity.status(HttpStatus.OK).body(getService().list());
+		verifyCache();
+		return ResponseEntity.status(HttpStatus.OK).body(getSingletron().getCache());
 	}
 	
 	@GetMapping("/{id}")
 	default ResponseEntity<?> getByID(@PathVariable("id") UUID ID) throws Exception {
-		return ResponseEntity.status(HttpStatus.OK).body(getService().getByID(ID));
+		verifyCache();
+		return ResponseEntity.status(HttpStatus.OK).body(getSingletron().getCache().stream().filter(data -> {
+			try {
+				Field idField = data.getClass().getSuperclass().getDeclaredField("id");
+				idField.setAccessible(true);
+				return idField.get(data).equals(ID);
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+			return false;
+		}));
 	}
 	
 	@PostMapping
 	default ResponseEntity<?> register(@RequestBody entity model){
+		updateCache();
 		return ResponseEntity.status(HttpStatus.OK).body(getService().save(model));
 	}
 	
 	@DeleteMapping("/{id}")
-	@ResponseStatus(code = HttpStatus.OK)
-	default void delete(@PathVariable("id") UUID ID){
+	default ResponseEntity<?> delete(@PathVariable("id") UUID ID){
+		updateCache();
 		getService().delete(ID);
+		return ResponseEntity.status(HttpStatus.OK).body(getSingletron().getCache());
 	}
 	
 	@PatchMapping("/{id}")
 	default ResponseEntity<?> edit(@PathVariable("id") UUID ID, @RequestBody entity model){
+		updateCache();
 		return ResponseEntity.status(HttpStatus.OK).body(getService().edit(ID,model));
+	}
+	
+	default void updateCache() {
+		if(getSingletron().getCache().isEmpty()) {
+			verifyCache();
+		}
+		getSingletron().setCache(getService().list());
+	}
+	
+	default void verifyCache() {
+		if(getSingletron().getCache().isEmpty()) {
+			getSingletron().setCache(getService().list());
+		}
 	}
 
 }
