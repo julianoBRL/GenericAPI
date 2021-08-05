7c7eff950584a86805c6ac1984bc4dac6136a4fe
Shintaro-PC <shintaro0dev@gmail.com>
2021-08-05 10:55:21 -0300

    added singletron

---
+++ src/main/java/com/shintaro/genericAPI/generic/Singletron.java
@@ -0,0 +1,25 @@
+package com.shintaro.genericAPI.generic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class Singletron<T extends Object> {
+	
+	private static Singletron<?> singletron;
+	
+	@Getter
+	@Setter
+	private List<T> cache = new ArrayList<>();
+	
+	public static <T> Singletron<?> getInstance() {
+		if(singletron == null) {
+			singletron = new Singletron<T>();
+		}
+		return singletron;
+	}
+
+}
